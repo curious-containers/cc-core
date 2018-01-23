e54f3c3171373429bc5e11ce4345e52ecee52c45
Christoph Jansen <Christoph.Jansen@htw-berlin.de>
2018-01-23 18:44:14 +0100

    cli_modes for less repetition in main files

--- cc_core/commons/schema_map.py
+++ cc_core/commons/schema_map.py
@@ -1,3 +1,5 @@
+from collections import OrderedDict
+
 from cc_core.commons.schemas.red import red_schema, red_inputs_schema, red_outputs_schema, red_jinja_schema
 from cc_core.commons.schemas.cwl import cwl_schema, cwl_job_schema
 from cc_core.commons.schemas.engines.container import container_engines
@@ -6,23 +8,23 @@ from cc_core.commons.schemas.engines.virtualization import virtualization_engine
 from cc_core.commons.schemas.connectors import connectors
 
 
-schemas = [
+schemas = OrderedDict([
     ('cwl', cwl_schema),
     ('cwl-job', cwl_job_schema),
     ('red', red_schema),
     ('red-jinja', red_jinja_schema),
     ('red-inputs', red_inputs_schema),
     ('red-outputs', red_outputs_schema)
-]
+])
 
 for e, s in connectors.items():
-    schemas.append(('red-connector-{}'.format(e), s))
+    schemas['red-connector-{}'.format(e)] = s
 
 for e, s in container_engines.items():
-    schemas.append(('red-container-{}'.format(e), s))
+    schemas['red-container-{}'.format(e)] = s
 
 for e, s in execution_engines.items():
-    schemas.append(('red-execution-{}'.format(e), s))
+    schemas['red-execution-{}'.format(e)] = s
 
 for e, s in virtualization_engines.items():
-    schemas.append(('red-virtualization-{}'.format(e), s))
+    schemas['red-virtualization-{}'.format(e)] = s
