be5b06e5b2e41122c30c1b266360587374056715
Bluemi <bruno.schilling@protonmail.ch>
2018-11-27 15:46:44 +0100

    added listing schema to faice schema {list,show}

--- cc_core/commons/schema_map.py
+++ cc_core/commons/schema_map.py
@@ -1,7 +1,7 @@
 from collections import OrderedDict
 
 from cc_core.commons.schemas.red import red_schema, fill_schema
-from cc_core.commons.schemas.cwl import cwl_schema, cwl_job_schema
+from cc_core.commons.schemas.cwl import cwl_schema, cwl_job_schema, cwl_job_listing_schema
 from cc_core.commons.schemas.engines.container import container_engines
 from cc_core.commons.schemas.engines.execution import execution_engines
 from cc_core.commons.schemas.connectors import connectors
@@ -11,7 +11,8 @@ schemas = OrderedDict([
     ('cwl', cwl_schema),
     ('cwl-job', cwl_job_schema),
     ('red', red_schema),
-    ('fill', fill_schema)
+    ('fill', fill_schema),
+    ('listing', cwl_job_listing_schema)
 ])
 
 for e, s in connectors.items():
