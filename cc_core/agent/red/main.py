4d4e527be7327d3e85fc74d0c96c063036b464ee
Christoph Jansen <Christoph.Jansen@htw-berlin.de>
2018-12-26 16:33:03 +0100

    improved --help

--- cc_core/agent/red/main.py
+++ cc_core/agent/red/main.py
@@ -41,7 +41,7 @@ def attach_args(parser):
     )
     parser.add_argument(
         '--format', action='store', type=str, metavar='FORMAT', choices=['json', 'yaml', 'yml'], default='yaml',
-        help='Specify meta data FORMAT as one of [json, yaml, yml]. Default is yaml.'
+        help='Specify FORMAT for generated data as one of [json, yaml, yml]. Default is yaml.'
     )
     parser.add_argument(
         '--leave-directories', action='store_true',
