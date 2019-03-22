a868c1ec805cd067976f81ebd34be0f2e8c82e49
Bluemi <bruno.schilling@protonmail.ch>
2019-03-22 16:38:09 +0100

    implemented command creation for red to blue

--- cc_core/commons/red_to_blue.py
+++ cc_core/commons/red_to_blue.py
@@ -1,62 +1,325 @@
 from enum import Enum
-from typing import List, Any, Union
+from functools import total_ordering
 
 import os.path
 
 import uuid
-from pprint import pprint
+
+from cc_core.commons.exceptions import JobSpecificationError
 
 
 def convert_red_to_blue(red_data):
     """
-    Converts the given red data into blue data.
-    The red data should not contain unresolved input references.
+    Converts the given red data into a list of blue data dictionary. The blue data is always given as list and each list
+    entry represents one batch in the red data.
+    The red data must not contain unresolved input references.
     :param red_data: The red data to convert
     :return: A dictionary containing the blue data
     """
     batches = extract_batches(red_data)
     complete_inputs(batches)
-    print('completed inputs:')
-    pprint(batches)
+    cli_description = red_data['cli']
+    cli_arguments = get_cli_arguments(cli_description['inputs'])
+    command = produce_base_command(cli_description.get('baseCommand'))
+    for batch in batches:
+        command = generate_command(command, cli_arguments, batch)
+
     return {}
 
 
-class CliArgument:
-    class CliArgumentType(Enum):
-        Positional = 0
-        Named = 1
+class InputType:
+    class InputCategory(Enum):
+        File = 0
+        Directory = 1
+        String = 2
+        Int = 3
+        Long = 4
+        Float = 5
+        Double = 6
+        Boolean = 7
 
-    def __init__(self, input_key, argument_type, input_type):
-        """
-        Creates a new CliArgument.
-        :param input_key: The input key of the cli argument
-        :param argument_type: The type of the cli argument (Positional, Named)
-        :param input_type: The type of the input key
-        """
-        self._input_key = input_key
-        self._argument_type = argument_type
-        self._input_type = input_type
+    def __init__(self, input_category, is_array, is_optional):
+        self.input_category = input_category
+        self._is_array = is_array
+        self._is_optional = is_optional
+
+    @staticmethod
+    def from_string(s):
+        is_optional = s.endswith('?')
+        if is_optional:
+            s = s[:-1]
+
+        is_array = s.endswith('[]')
+        if is_array:
+            s = s[:-2]
+
+        input_category = None
+        for ic in InputType.InputCategory:
+            if s == ic.name:
+                input_category = ic
+
+        assert input_category is not None
+
+        return InputType(input_category, is_array, is_optional)
+
+    def to_string(self):
+        return '{}{}{}'.format(self.input_category.name,
+                               '[]' if self._is_array else '',
+                               '?' if self._is_optional else '')
+
+    def __repr__(self):
+        return self.to_string()
+
+    def __eq__(self, other):
+        return (self.input_category == other.input_category) and\
+               (self._is_array == other.is_array()) and\
+               (self._is_optional == other.is_optional())
+
+    def is_file(self):
+        return self.input_category == InputType.InputCategory.File
+
+    def is_directory(self):
+        return self.input_category == InputType.InputCategory.Directory
+
+    def is_array(self):
+        return self._is_array
+
+    def is_optional(self):
+        return self._is_optional
 
 
-def generate_command(cli_description, batch):
+def generate_command(base_command, cli_arguments, batch):
     """
     Creates a command from the cli description and a given batch.
-    :param cli_description: The cli description of the executed command
+    :param base_command: The base command to use
+    :param cli_arguments: The arguments of the described tool
     :param batch: The batch to execute
     :return: A list of string representing the created command
     """
-    command = produce_base_command(cli_description.get('baseCommand'))
+    command = base_command.copy()
 
-    arguments = get_arguments(cli_description['inputs'])
+    execution_arguments = []
+
+    for cli_argument in cli_arguments:
+        batch_value = batch['inputs'].get(cli_argument.input_key)
+        execution_argument = create_execution_argument(cli_argument, batch_value)
+        execution_arguments.extend(execution_argument)
+
+    command.extend(execution_arguments)
+
+    print('command: {}'.format(command))
+
+    return command
 
 
-def get_arguments(cli_inputs):
+INPUT_CATEGORY_REPRESENTATION_MAPPER = {
+    InputType.InputCategory.File: lambda batch_value: batch_value['path'],
+    InputType.InputCategory.Directory: lambda batch_value: batch_value['path'],
+    InputType.InputCategory.String: lambda batch_value: batch_value,
+    InputType.InputCategory.Int: lambda batch_value: str(batch_value),
+    InputType.InputCategory.Long: lambda batch_value: str(batch_value),
+    InputType.InputCategory.Float: lambda batch_value: str(batch_value),
+    InputType.InputCategory.Double: lambda batch_value: str(batch_value),
+    InputType.InputCategory.Boolean: lambda batch_value: str(batch_value)
+}
+
+
+def create_execution_argument(cli_argument, batch_value):
     """
-    Returns a list of arguments
-    :param cli_inputs:
-    :return:
+    Creates a string representing an execution argument. Like '--outdir=/path/to/file'
+    :param cli_argument: The cli argument
+    :param batch_value: The batch value corresponding to the cli argument. Can be None
+    :return: A list of strings, that can be used to extend the command. Returns an empty list if (cli_argument is
+    optional and batch_value is None) or (cli argument is array and len(batch_value) is 0)
+    :raise JobSpecificationError: If cli argument is mandatory, but batch value is None
     """
-    raise NotImplementedError()
+    # handle optional arguments
+    if batch_value is None:
+        if cli_argument.is_optional():
+            return []
+        else:
+            raise JobSpecificationError('required argument "{}" is missing'.format(cli_argument.input_key))
+
+    # handle arrays (create argument list)
+    argument_list = []
+    if cli_argument.is_array():
+        assert isinstance(batch_value, list)
+        for sub_batch_value in batch_value:
+            r = INPUT_CATEGORY_REPRESENTATION_MAPPER[cli_argument.get_type_category()](sub_batch_value)
+            argument_list.append(r)
+
+        if not argument_list:
+            return None
+    else:
+        argument_list.append(INPUT_CATEGORY_REPRESENTATION_MAPPER[cli_argument.get_type_category()](batch_value))
+
+    # join argument list
+    if cli_argument.item_separator:
+        joined_arguments = cli_argument.item_separator.join(argument_list)
+
+        # add prefix
+        if cli_argument.prefix:
+            if cli_argument.separate:
+                argument_list = [cli_argument.prefix, joined_arguments]
+            else:
+                argument_list = ['{}{}'.format(cli_argument.prefix, joined_arguments)]
+    else:
+        # add prefix
+        # if item separator is not given, always separate prefix from argument list
+        if cli_argument.prefix:
+            argument_list.insert(0, cli_argument.prefix)
+
+    return argument_list
+
+
+@total_ordering
+class CliArgumentPosition:
+    class CliArgumentPositionType(Enum):
+        Positional = 0
+        Named = 1
+
+    def __init__(self, argument_position_type, binding_position):
+        """
+        Creates a new CliArgumentPosition.
+        :param argument_position_type: The position type of this argument position
+        """
+        self.argument_position_type = argument_position_type
+        self.binding_position = binding_position
+
+    @staticmethod
+    def new_positional_argument(binding_position):
+        """
+        Creates a new positional argument position.
+        :param binding_position: The input position of the argument
+        :return: A new CliArgumentPosition with position_type Positional
+        """
+        return CliArgumentPosition(CliArgumentPosition.CliArgumentPositionType.Positional, binding_position)
+
+    @staticmethod
+    def new_named_argument():
+        """
+        Creates a new named argument position.
+        :return: A new CliArgumentPosition with position_type Named.
+        """
+        return CliArgumentPosition(CliArgumentPosition.CliArgumentPositionType.Named, 0)
+
+    def is_positional(self):
+        return self.argument_position_type is CliArgumentPosition.CliArgumentPositionType.Positional
+
+    def is_named(self):
+        return self.argument_position_type is CliArgumentPosition.CliArgumentPositionType.Named
+
+    def __eq__(self, other):
+        return (self.argument_position_type is other.argument_position_type) \
+               and (self.binding_position == other.binding_position)
+
+    def __lt__(self, other):
+        if self.argument_position_type is CliArgumentPosition.CliArgumentPositionType.Positional:
+            if other.argument_position_type is CliArgumentPosition.CliArgumentPositionType.Positional:
+                return self.binding_position < other.binding_position
+            else:
+                return True
+        else:
+            return False
+
+    def __repr__(self):
+        return 'CliArgumentPosition(argument_position_type={}, binding_position={})'\
+               .format(self.argument_position_type, self.binding_position)
+
+
+class CliArgument:
+    def __init__(self, input_key, argument_position, input_type, prefix, separate, item_separator):
+        """
+        Creates a new CliArgument.
+        :param input_key: The input key of the cli argument
+        :param argument_position: The type of the cli argument (Positional, Named)
+        :param input_type: The type of the input key
+        :param prefix: The prefix to prepend to the value
+        :param separate: Separate prefix and value
+        :param item_separator: The string to join the elements of an array
+        """
+        self.input_key = input_key
+        self.argument_position: CliArgumentPosition = argument_position
+        self.input_type: InputType = input_type
+        self.prefix = prefix
+        self.separate = separate
+        self.item_separator = item_separator
+
+    def __repr__(self):
+        return 'CliArgument(\n\t{}\n)'.format('\n\t'.join(['input_key={}'.format(self.input_key),
+                                                           'argument_position={}'.format(self.argument_position),
+                                                           'input_type={}'.format(self.input_type),
+                                                           'prefix={}'.format(self.prefix),
+                                                           'separate={}'.format(self.separate),
+                                                           'item_separator={}'.format(self.item_separator)]))
+
+    @staticmethod
+    def new_positional_argument(input_key, input_type, input_binding_position, item_separator):
+        return CliArgument(input_key=input_key,
+                           argument_position=CliArgumentPosition.new_positional_argument(input_binding_position),
+                           input_type=input_type,
+                           prefix=None,
+                           separate=False,
+                           item_separator=item_separator)
+
+    @staticmethod
+    def new_named_argument(input_key, input_type, prefix, separate, item_separator):
+        return CliArgument(input_key=input_key,
+                           argument_position=CliArgumentPosition.new_named_argument(),
+                           input_type=input_type,
+                           prefix=prefix,
+                           separate=separate,
+                           item_separator=item_separator)
+
+    def is_array(self):
+        return self.input_type.is_array()
+
+    def is_optional(self):
+        return self.input_type.is_optional()
+
+    def is_positional(self):
+        return self.argument_position.is_positional()
+
+    def is_named(self):
+        return self.argument_position.is_named()
+
+    def get_type_category(self):
+        return self.input_type.input_category
+
+    @staticmethod
+    def from_cli_input_description(input_key, cli_input_description):
+        """
+        Creates a new CliArgument depending of the information given in the cli input description.
+        inputBinding keys = 'prefix' 'separate' 'position' 'itemSeparator'
+        :param input_key: The input key of the cli input description
+        :param cli_input_description: red_data['cli']['inputs'][input_key]
+        :return: A new CliArgument
+        """
+        input_binding = cli_input_description['inputBinding']
+        input_binding_position = input_binding.get('position', 0)
+        prefix = input_binding.get('prefix')
+        separate = input_binding.get('prefix', True)
+        item_separator = input_binding.get('itemSeparator')
+
+        input_type = InputType.from_string(cli_input_description['type'])
+
+        if prefix:
+            arg = CliArgument.new_named_argument(input_key, input_type, prefix, separate, item_separator)
+        else:
+            arg = CliArgument.new_positional_argument(input_key, input_type, input_binding_position, item_separator)
+        return arg
+
+
+def get_cli_arguments(cli_inputs):
+    """
+    Returns a sorted list of cli arguments.
+    :param cli_inputs: The cli inputs description
+    :return: A list of CliArguments
+    """
+    cli_arguments = []
+    for input_key, cli_input_description in cli_inputs.items():
+        cli_arguments.append(CliArgument.from_cli_input_description(input_key, cli_input_description))
+    return sorted(cli_arguments, key=lambda cli_argument: cli_argument.argument_position)
 
 
 def produce_base_command(cwl_base_command):
@@ -73,7 +336,6 @@ def produce_base_command(cwl_base_command):
         base_command = []
 
     return base_command
-
 
 
 def complete_inputs(batches):
@@ -164,5 +426,5 @@ def extract_batches(red_data):
         return batches
     else:
         batch = {'inputs': red_data['inputs'],
-                 'outputs': red_data['outputs']}
+                 'outputs': red_data.get('outputs')}
         return [batch]
